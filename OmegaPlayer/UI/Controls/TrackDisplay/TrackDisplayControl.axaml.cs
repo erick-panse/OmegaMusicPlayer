e25ea3c4da24e72a563e69bebac9c1cdfe34877d
Enp256 <ericknp2010@hotmail.com>
2024-12-26 17:04:02 -0300

    right-click on tracks in library view and flyout style

--- OmegaPlayer/UI/Controls/TrackDisplay/TrackDisplayControl.axaml.cs
+++ OmegaPlayer/UI/Controls/TrackDisplay/TrackDisplayControl.axaml.cs
@@ -7,6 +7,10 @@ using OmegaPlayer.Features.Library.Models;
 using OmegaPlayer.Features.Library.ViewModels;
 using MsBox.Avalonia.Enums;
 using MsBox.Avalonia;
+using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
+using System.Linq;
+using Avalonia.Media;
 
 namespace OmegaPlayer.UI.Controls.TrackDisplay
 {
@@ -43,14 +47,81 @@ namespace OmegaPlayer.UI.Controls.TrackDisplay
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
-
             if (e.NameScope.Find<ItemsControl>("PART_ItemsControl") is ItemsControl itemsControl)
             {
                 _itemsControl = itemsControl;
                 itemsControl.AddHandler(InputElement.PointerReleasedEvent, ArtistClicked, RoutingStrategies.Tunnel);
                 itemsControl.AddHandler(InputElement.PointerEnteredEvent, Track_PointerEntered, RoutingStrategies.Tunnel);
                 itemsControl.AddHandler(InputElement.PointerExitedEvent, Track_PointerExited, RoutingStrategies.Tunnel);
+
+                // Subscribe to ItemsControl's LayoutUpdated event
+                itemsControl.LayoutUpdated += ItemsControl_LayoutUpdated;
             }
+        }
+
+        private void ItemsControl_LayoutUpdated(object sender, System.EventArgs e)
+        {
+            var borders = _itemsControl.GetVisualDescendants()
+                .OfType<Border>()
+                .Where(b => b.Classes.Contains("trackPanel") && b.ContextMenu == null);
+
+            foreach (var border in borders)
+            {
+                AttachContextMenu(border);
+            }
+        }
+
+        private void AttachContextMenu(Border border)
+        {
+            var viewModel = this.DataContext as LibraryViewModel;
+            if (viewModel == null) return;
+
+            var track = border.DataContext as TrackDisplayModel;
+            if (track == null) return;
+
+            var contextMenu = new ContextMenu
+            {
+                DataContext = track // Set the track as the DataContext for the context menu
+            };
+
+            var playMenuItem = new MenuItem
+            {
+                Header = "Play",
+                Command = viewModel.PlayTrackCommand,
+                CommandParameter = track
+            };
+            playMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("PlayIcon") as Geometry };
+
+            var addToQueueMenuItem = new MenuItem
+            {
+                Header = "Add to Queue",
+                Command = viewModel.AddToQueueCommand,
+                CommandParameter = track
+            };
+            addToQueueMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("AddTrackIcon") as Geometry };
+
+            var addAsNextMenuItem = new MenuItem
+            {
+                Header = "Add as Next",
+                Command = viewModel.AddAsNextTracksCommand,
+                CommandParameter = track
+            };
+            addAsNextMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("AddTrackIcon") as Geometry };
+
+            var openAlbumMenuItem = new MenuItem
+            {
+                Header = "Go to Album",
+                Command = viewModel.OpenAlbumCommand,
+                CommandParameter = track.AlbumID
+            };
+
+            contextMenu.Items.Add(playMenuItem);
+            contextMenu.Items.Add(addToQueueMenuItem);
+            contextMenu.Items.Add(addAsNextMenuItem);
+            contextMenu.Items.Add(new Separator());
+            contextMenu.Items.Add(openAlbumMenuItem);
+
+            border.ContextMenu = contextMenu;
         }
 
         private void ArtistClicked(object? sender, PointerReleasedEventArgs e)
@@ -84,8 +155,6 @@ namespace OmegaPlayer.UI.Controls.TrackDisplay
                 track.IsPointerOver = false;
             }
         }
-
-
 
         private async void ShowMessageBox(string message)
         {
---
+++ OmegaPlayer/UI/Helpers/TrackContextMenuHelper.cs
@@ -0,0 +1,82 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using OmegaPlayer.Features.Library.Models;
+using OmegaPlayer.Features.Library.ViewModels;
+using System.Windows.Input;
+
+namespace OmegaPlayer.UI.Controls.Helpers
+{
+    public class TrackContextMenuHelper : AvaloniaObject
+    {
+        public static readonly AttachedProperty<ICommand> PlayCommandProperty =
+            AvaloniaProperty.RegisterAttached<TrackContextMenuHelper, Border, ICommand>("PlayCommand");
+
+        public static readonly AttachedProperty<ICommand> AddToQueueCommandProperty =
+            AvaloniaProperty.RegisterAttached<TrackContextMenuHelper, Border, ICommand>("AddToQueueCommand");
+
+        public static readonly AttachedProperty<ICommand> AddAsNextCommandProperty =
+            AvaloniaProperty.RegisterAttached<TrackContextMenuHelper, Border, ICommand>("AddAsNextCommand");
+
+        public static readonly AttachedProperty<ICommand> OpenAlbumCommandProperty =
+            AvaloniaProperty.RegisterAttached<TrackContextMenuHelper, Border, ICommand>("OpenAlbumCommand");
+
+        public static void SetPlayCommand(Border element, ICommand value)
+            => element.SetValue(PlayCommandProperty, value);
+
+        public static ICommand GetPlayCommand(Border element)
+            => element.GetValue(PlayCommandProperty);
+
+        public static void SetAddToQueueCommand(Border element, ICommand value)
+            => element.SetValue(AddToQueueCommandProperty, value);
+
+        public static ICommand GetAddToQueueCommand(Border element)
+            => element.GetValue(AddToQueueCommandProperty);
+
+        public static void SetAddAsNextCommand(Border element, ICommand value)
+            => element.SetValue(AddAsNextCommandProperty, value);
+
+        public static ICommand GetAddAsNextCommand(Border element)
+            => element.GetValue(AddAsNextCommandProperty);
+
+        public static void SetOpenAlbumCommand(Border element, ICommand value)
+            => element.SetValue(OpenAlbumCommandProperty, value);
+
+        public static ICommand GetOpenAlbumCommand(Border element)
+            => element.GetValue(OpenAlbumCommandProperty);
+
+        public static void AttachContextMenu(Border border, LibraryViewModel viewModel)
+        {
+            var contextMenu = new ContextMenu();
+
+            var playMenuItem = new MenuItem { Header = "Play" };
+            playMenuItem.Command = viewModel.PlayTrackCommand;
+            playMenuItem.CommandParameter = border.DataContext;
+            playMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("PlayIcon") as Geometry };
+
+            var addToQueueMenuItem = new MenuItem { Header = "Add to Queue" };
+            addToQueueMenuItem.Command = viewModel.AddToQueueCommand;
+            addToQueueMenuItem.CommandParameter = border.DataContext;
+            addToQueueMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("AddTrackIcon") as Geometry };
+
+            var addAsNextMenuItem = new MenuItem { Header = "Add as Next" };
+            addAsNextMenuItem.Command = viewModel.AddAsNextTracksCommand;
+            addAsNextMenuItem.CommandParameter = border.DataContext;
+            addAsNextMenuItem.Icon = new PathIcon { Data = Application.Current.FindResource("AddTrackIcon") as Geometry };
+
+            var separator = new Separator();
+
+            var openAlbumMenuItem = new MenuItem { Header = "Go to Album" };
+            openAlbumMenuItem.Command = viewModel.OpenAlbumCommand;
+            openAlbumMenuItem.CommandParameter = (border.DataContext as TrackDisplayModel)?.AlbumID;
+
+            contextMenu.Items.Add(playMenuItem);
+            contextMenu.Items.Add(addToQueueMenuItem);
+            contextMenu.Items.Add(addAsNextMenuItem);
+            contextMenu.Items.Add(separator);
+            contextMenu.Items.Add(openAlbumMenuItem);
+
+            border.ContextMenu = contextMenu;
+        }
+    }
+}
