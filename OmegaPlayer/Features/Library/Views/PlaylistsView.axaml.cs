5ed826a1ddbb5bd0820f7cc3b94ed1fea88c55d7
Enp256 <ericknp2010@hotmail.com>
2025-03-10 11:08:17 -0300

    removing load more on scroll of playlist, since it already loads all playlists

--- OmegaPlayer/Features/Library/Views/PlaylistsView.axaml.cs
+++ OmegaPlayer/Features/Library/Views/PlaylistsView.axaml.cs
@@ -12,25 +12,5 @@ namespace OmegaPlayer.Features.Library.Views
             InitializeComponent();
             ViewModelLocator.AutoWireViewModel(this);
         }
-
-        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            if (sender != null)
-            {
-                var scrollViewer = sender as ScrollViewer;
-
-                // If the user scrolls near the end, trigger the load more command
-                if (scrollViewer.Offset.Y >= scrollViewer.Extent.Height - scrollViewer.Viewport.Height - 100)
-                {
-                    // Get the current view's ViewModel
-                    if (DataContext is PlaylistsViewModel PlaylistViewModel &&
-                        PlaylistViewModel is ILoadMoreItems loadMoreItems &&
-                        loadMoreItems.LoadMoreItemsCommand.CanExecute(null))
-                    {
-                        loadMoreItems.LoadMoreItemsCommand.Execute(null);
-                    }
-                }
-            }
-        }
     }
 }
