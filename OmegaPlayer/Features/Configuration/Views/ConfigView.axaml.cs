4726067b892bb7be8e37aaf8d572d4f8939e41e2
Enp256 <ericknp2010@hotmail.com>
2025-03-12 16:43:12 -0300

    Creating  Custom ComboBox to prevent unwanted Scroll caused by Avalonia's TryFocusSelectedItem() in the ComboBox control

--- OmegaPlayer/Features/Configuration/Views/ConfigView.axaml.cs
+++ OmegaPlayer/Features/Configuration/Views/ConfigView.axaml.cs
@@ -1,18 +1,23 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Microsoft.Extensions.DependencyInjection;
 using OmegaPlayer.Features.Configuration.ViewModels;
 using OmegaPlayer.UI;
 using OmegaPlayer.Features.Library.Services;
 using CommunityToolkit.Mvvm.Messaging;
-using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using OmegaPlayer.Core.Services;
 using OmegaPlayer.Infrastructure.Services;
+using System;
 
 namespace OmegaPlayer.Features.Configuration.Views
 {
     public partial class ConfigView : UserControl
     {
+        private double _savedScrollPosition = 0;
+        private ScrollViewer _scrollViewer;
+
         public ConfigView()
         {
             InitializeComponent();
@@ -35,11 +40,72 @@ namespace OmegaPlayer.Features.Configuration.Views
                 blacklistService,
                 profileManager,
                 profileConfigService,
-                globalConfigService, 
+                globalConfigService,
                 localizationService,
                 messenger,
                 storageProvider
             );
+
+            // Save a reference to the ScrollViewer for easier access
+            this.AttachedToVisualTree += ConfigView_AttachedToVisualTree;
+        }
+
+        private void ConfigView_AttachedToVisualTree(object sender, VisualTreeAttachmentEventArgs e)
+        {
+            _scrollViewer = this.FindControl<ScrollViewer>("ConfigScrollViewer");
+
+            // Ensure it won't automatically scroll on focus
+            if (_scrollViewer != null)
+            {
+                _scrollViewer.BringIntoViewOnFocusChange = false;
+            }
+        }
+
+        // Event handlers for CustomComboBox controls
+        private void ComboBox_GotFocus(object sender, GotFocusEventArgs e)
+        {
+            if (_scrollViewer != null)
+            {
+                // Save current scroll position
+                _savedScrollPosition = _scrollViewer.Offset.Y;
+            }
+        }
+
+        private void ComboBox_DropDownOpened(object sender, EventArgs e)
+        {
+            if (_scrollViewer != null)
+            {
+                // Immediately restore saved position
+                _scrollViewer.Offset = new Vector(_scrollViewer.Offset.X, _savedScrollPosition);
+
+                // Attach event handler to restore scroll position if it changes
+                _scrollViewer.PropertyChanged += ScrollViewer_PropertyChanged;
+            }
+        }
+
+        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            if (_scrollViewer != null)
+            {
+                // Remove the property change handler
+                _scrollViewer.PropertyChanged -= ScrollViewer_PropertyChanged;
+
+                // Restore to the saved position again
+                _scrollViewer.Offset = new Vector(_scrollViewer.Offset.X, _savedScrollPosition);
+            }
+        }
+
+        private void ScrollViewer_PropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            // If the offset changed, restore it to our saved value
+            if (e.Property == ScrollViewer.OffsetProperty && sender is ScrollViewer scrollViewer)
+            {
+                var newOffset = (Vector)e.NewValue;
+                if (Math.Abs(newOffset.Y - _savedScrollPosition) > 0.1)
+                {
+                    scrollViewer.Offset = new Vector(newOffset.X, _savedScrollPosition);
+                }
+            }
         }
     }
 }
